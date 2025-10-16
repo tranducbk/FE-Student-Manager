0074f6b25babd77d97e4dc190342c8c899135211
DucTA <anhtd@avadagroup.com>
2025-10-16 15:15:09 +0700

    Thay đổi trường nhập ảnh đại diện từ file upload sang URL, cải thiện trải nghiệm người dùng với placeholder hướng dẫn nhập URL ảnh đại diện.

--- src/app/admin/[adminId]/page.jsx
+++ src/app/admin/[adminId]/page.jsx
@@ -889,10 +889,11 @@ const UserProfile = ({ params }) => {
                           </label>
                           <input
                             className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            type="file"
-                            name="avatar"
-                            accept="image/*"
+                            type="url"
+                            id="avatar"
+                            value={formData.avatar}
                             onChange={handleChange}
+                            placeholder="Nhập URL ảnh đại diện"
                           />
                         </div>
                       </div>
